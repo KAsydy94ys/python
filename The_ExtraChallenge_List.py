3f343dd9f103a911571a9d2bea0672c948aacbd1
KAsydy94ys <kayson.phamgia@gmail.com>
2025-01-23 20:07:24 +0700

    The_ExtraChallenge_List.py

--- The_ExtraChallenge_List.py
+++ The_ExtraChallenge_List.py
@@ -8,7 +8,8 @@ while True:
     print ("1) Add another number")
     print ("2) Remove a number")
     print ("3) Change a number")
-    print ("4) Display the list")
+    print ("4) Sum of all numbers")
+    print ("5) Print the list, but separate even and odd")
     question = input("Enter your choice: ")
     if question == '1':
         addNumber = int(input("What number do you wanna add then: "))
@@ -26,14 +27,25 @@ while True:
                 print("The number you want to remove is not in there dumbo")
                 continue
     if question == '3':
-        editPosition = int(input("Enter the position of the number inside the list: "))
-        editNumber = int(input("Enter the new value you want: "))
-        numberList[editPosition] = editNumber
-        print(numberList)
+        while True:    
+            editPosition = int(input("Enter the position of the number inside the list: "))
+            if editPosition > len(numberList) - 1:
+                print ("That position is incorrect")
+                continue
+            else:
+                editNumber = int(input("Enter the new value you want: "))
+                numberList[editPosition] = editNumber
+                print(numberList)
     elif question == '4':
         boom= sum(numberList)
         print(boom)
-        
-    
-
-    
+    if question == '5':
+        evenList = []
+        oddList = []
+        for i in numberList:
+            if i % 2 == 0:
+                evenList.append(i)
+            else:
+                oddList.append(i)
+        print ("Even number list:", evenList)
+        print ("Odd number list:", oddList)         
