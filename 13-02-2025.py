839170894bfe746d573fac2c0215951ea483f9c0
KAsydy94ys <kayson.phamgia@gmail.com>
2025-02-27 20:21:15 +0700

    27-02-2025.py

--- 13-02-2025.py
+++ 13-02-2025.py
@@ -1,3 +1,21 @@
+def findFactors():
+    factors = []
+    divisor = int(input("Enter a number: "))
+    print ("The factors of that number is: ")
+    for i in range(1, divisor + 1):
+        if divisor % i == 0:
+            factors.append(i)
+    print (factors)
+
+def hangman():
+    word = "qewrtyuioafsfkfl"
+    array_inside = []
+    while True:
+        letter = input("Enter a letter or the word: ")
+        if letter in word:
+            print("true")
+        else:
+            print("false")
 
 def orderString():
     string = input("Enter a word, use uppercase and lowercase pleaseee: ")
@@ -38,10 +56,25 @@ def antiVowel():
             detector = detector.replace(c, '')
     print(detector)
 
+art = """
+ _______  _______  ______   _______    _______  _______  _        _        _______  _______ __________________ _______  _       
+(  ____ \(  ___  )(  __  \ (  ____ \  (  ____ \(  ___  )( \      ( \      (  ____ \(  ____ \\__   __/\__   __/(  ___  )( (    /|
+| (    \/| (   ) || (  \  )| (    \/  | (    \/| (   ) || (      | (      | (    \/| (    \/   ) (      ) (   | (   ) ||  \  ( |
+| |      | |   | || |   ) || (__      | |      | |   | || |      | |      | (__    | |         | |      | |   | |   | ||   \ | |
+| |      | |   | || |   | ||  __)     | |      | |   | || |      | |      |  __)   | |         | |      | |   | |   | || (\ \) |
+| |      | |   | || |   ) || (        | |      | |   | || |      | |      | (      | |         | |      | |   | |   | || | \   |
+| (____/\| (___) || (__/  )| (____/\  | (____/\| (___) || (____/\| (____/\| (____/\| (____/\   | |   ___) (___| (___) || )  \  |
+(_______/(_______)(______/ (_______/  (_______/(_______)(_______/(_______/(_______/(_______/   )_(   \_______/(_______)|/    )_)
+"""
+
+
 while True:
-    print ("To activate orderString, type 1")
-    print ("To activate positioningFirstMiddleLast, type 2")
+    print(art)
+    print ("To order strings from lower case to uppercase, type 1")
+    print ("To take only the first middle and last letter from a string, type 2")
     print ("To activate antiVowel, type 3")
+    print ("To find divisors of an interger, type 4")
+    print ("To play hangman, type 5")
     print ("To stop the code, press n")
     option_choice = input("Enter your choice: ")
     if option_choice == '1':
@@ -50,8 +83,15 @@ while True:
         positioningFirstMiddleLast()
     elif option_choice == '3':
         antiVowel()
+    elif option_choice == '4':
+        findFactors()
+    elif option_choice == '5':
+        hangman()
     elif option_choice == 'n':
         break
     else:
         print("That is not an option, try again")
+
+
+
 
